3b990574e8269028040e374d8cfb334a147c1fb9
AhmedSedeek <ahmedsedeek2007@gmail.com>
2016-08-30 14:40:44 +0200

    Initial commit

---
+++ app/src/main/java/layout/MainFragement.java
@@ -0,0 +1,33 @@
+package layout;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.sunshine.R;
+
+import java.util.zip.Inflater;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainFragement extends Fragment {
+
+
+    public MainFragement() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+
+}
